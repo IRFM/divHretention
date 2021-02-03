db4e46b9a80f168cc43e6784c4ee6eddb425da3e
remdelaportemathurin <delaporte.mathurin@laposte.net>
2021-02-03 10:16:48 +0100

    removed real parts + only 1D arrays

--- extract_WEST_data.py
+++ extract_WEST_data.py
@@ -9,38 +9,46 @@ import numpy as np
 def extract_data(filename):
     if filename.endswith("mat"):
         data = scio.loadmat(filename)
-        R = data['R_wall']
-        Z = data['Z_wall']
-        Eion = data['Eion']
-        Eatom = data['Eatom']
-        ion_flux = data['ion_flux']
-        atom_flux = data['atom_flux']
-        net_heat_flux = data['net_heat_flux']
-        ind_target = data['ind_target']
+        R = np.hstack(data['R_wall'])
+        Z = np.hstack(data['Z_wall'])
+        Eion = np.hstack(data['Eion'])
+        Eatom = np.hstack(data['Eatom'])
+        ion_flux = np.hstack(data['ion_flux'])
+        atom_flux = np.hstack(data['atom_flux'])
+        net_heat_flux = np.hstack(data['net_heat_flux'])
+        ind_target = np.hstack(data['ind_target'])
+        angles_ions = np.hstack(data["target_strike_angle"])
+        angles_atoms = np.hstack(data["target_strike_angle"])  # TODO verify
+        index_start = ind_target[0]
+        index_stop = ind_target[-1]
+        R_div = R[index_start:index_stop]
+        Z_div = Z[index_start:index_stop]
+        E_ion_div = Eion[index_start:index_stop]
+        E_atom_div = Eatom[index_start:index_stop]
+        ion_flux_div = ion_flux[index_start:index_stop]
+        atom_flux_div = atom_flux[index_start:index_stop]
+        net_heat_flux_div = net_heat_flux[index_start:index_stop]
+
+        # arc length for a straight line
+        arc_length_div = ((R_div - R_div[0])**2 + (Z_div - Z_div[0])**2)**0.5
     elif filename.endswith(("txt", "csv")):
         data = np.genfromtxt(filename, delimiter=";", names=True)
-        R = data["R_wallm"]
-        E = data["EioneV"]
-        Z = data["Z_wallm"]
+        R_div, Z_div = [], []
+        arc_length_div = data["s_cell"] - data["s_cell"][0]
+        E_ion_div = data["E_imp_ion"]
+        E_atom_div = data["E_imp_atom"]
+        angles_ions = data["alpha_V_ion"]
+        angles_atoms = data["alpha_V_atom"]
+        ion_flux_div = data["flux_imp_ion"]
+        atom_flux_div = data["flux_imp_atom"]
+        net_heat_flux_div = data["net_energy_flux"]
 
-    index_start = ind_target[0][0]
-    index_stop = ind_target[-1][0]
-    R_div = R[index_start:index_stop]
-    Z_div = Z[index_start:index_stop]
-    E_ion_div = Eion[index_start:index_stop]
-    E_atom_div = Eatom[index_start:index_stop]
-    ion_flux_div = ion_flux[index_start:index_stop]
-    atom_flux_div = atom_flux[index_start:index_stop]
-    net_heat_flux_div = net_heat_flux[index_start:index_stop]
-
-    # arc length for a straight line
-    arc_length_div = ((R_div - R_div[0])**2 + (Z_div - Z_div[0])**2)**0.5
     return R_div, Z_div, arc_length_div, E_ion_div, E_atom_div, ion_flux_div, \
-        atom_flux_div, net_heat_flux_div, data
+        atom_flux_div, net_heat_flux_div, angles_ions, angles_atoms, data
 
 
 if __name__ == "__main__":
-    filename = "data/exposure_conditions_divertor/WEST/Hao/wall_data.mat"
+    filename = "data/exposure_conditions_divertor/WEST/Hao/P1.0e21_wall_data.mat"
     R_div, Z_div, arc_length_div, E_ion_div, E_atom_div, ion_flux_div, \
         atom_flux_div, net_heat_flux_div, data = extract_data(filename)
     plt.figure()
