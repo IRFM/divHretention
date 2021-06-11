2e423f8cada1d6cff7a553c0b7d685940b65538d
remdelaportemathurin <delaporte.mathurin@laposte.net>
2021-06-11 10:47:32 +0200

    after import

--- divHretention/__init__.py
+++ divHretention/__init__.py
@@ -1,7 +1,5 @@
 from .data.list_of_files import list_of_high_temp_files, list_of_low_temp_files
 
-step_mb = 6
-
 from .compute_implantation_coefficients_angle import \
     implantation_range, reflection_coeff
 from .inventory_T_c import estimate_inventory_with_gp_regression
@@ -15,3 +13,6 @@ from .plotting.plot_2d_contour import plot_Tc_map_with_subplots, \
 from .plotting.plot_along_divertor import \
     plot_T_c_inv_along_divertor, plot_particle_exposure_along_divertor, \
     plot_along_divertor, create_correspondance_dict
+
+
+step_mb = 6
